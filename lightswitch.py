ac852be605c1b89c5e5aab0cf1860967f8a3ce61
oldmartijntje <oldmartijntje@gmai.com>
2022-04-07 10:08:26 +0200

    e

---
+++ lightswitch.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+
+logFile = open('actions.log', 'w')
+
+
+def aanUit():
+    global lichtAanOfUit
+    if lichtAanOfUit == True:
+        lichtAanOfUit = False
+        print("light is off")
+        button["text"] = "Switch Light On"
+        window["bg"] = "black"
+        logFile.write("light is off\n")
+    elif lichtAanOfUit == False:
+        lichtAanOfUit = True
+        print("light is on")
+        button["text"] = "Switch Light Off"
+        window["bg"] = "yellow"
+        logFile.write("light is on\n")
+    
+window = tk.Tk()
+window.geometry('300x200')
+window.configure(bg='yellow')
+button = tk.Button(text='Switch Light Off', bg="white", fg="black", command=aanUit)
+button.pack(pady = 20, padx = 20)
+
+# schijf hier tussen je code
+window.title("what if i don't want the title to be TK?")
+lichtAanOfUit = True
+# schijf hier tussen je code
+
+window.mainloop()
+logFile.close()
